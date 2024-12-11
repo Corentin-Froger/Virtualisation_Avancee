401671a0b078c2fb10b1bd624a07594748f8426d
Corentin <corentin.froger@netc.fr>
2024-12-11 17:12:46 +0100

    Modif index.php

--- TP6/index.php
+++ TP6/index.php
@@ -1,2 +1,16 @@
 <?php
-echo "GRAAAAAAAAAAH";
+
+
+$databaseHost = $_GET["server"];
+$databaseUsername = "root";
+$databasePassword = "root";
+//$databaseName = 'rolodex';
+
+echo $databaseHost;
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword); 
+
+echo $mysqli;
+
+?>
